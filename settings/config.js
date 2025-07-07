74bd5445b5694a10df39ef5ad5c456a19ea489b5
DARK SHADOW <pubuduchinthaka558@gmail.com>
2025-07-07 06:06:59 +0530

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,19 @@
+//================================//
+// Main
+//================================//
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['62']
+//================================//
+// Sticker
+//================================//
+global.packname = "Sticker By RizxVelz"
+global.author = "RizxvelzOfficial"
+//================================//
+// Global Mess
+//================================//
+global.menuMode = global.menuMode || 'nobutton';
+global.mess = {
+ owner: '*You are not the owner*',
+ premium: '*You are not premium*',
+ group: '*This feature is for groups only*'
+}
